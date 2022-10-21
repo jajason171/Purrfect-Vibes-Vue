41f8e506220783226813362c0bf3551c0e496594
DevCP13 <54089746+jajason171@users.noreply.github.com>
2022-10-22 00:16:59 +0700

    Fist push

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": [
+      "error",
+      { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+html {
+  height: -webkit-fill-available; /* We have to fix html height */
+}
+
+body {
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+}
+nav {
+  padding: 30px;
+  background-color: #339268;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #339268;
+    }
+  }
+  height: 10vh;
+  max-height: 150px;
+  /* @media (max-width: 768px) {
+    height: 10%;
+  } */
+}
+</style>

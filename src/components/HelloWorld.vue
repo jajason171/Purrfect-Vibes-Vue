a7eb99783732e0fe0e41f0e0627a2279ed09aaa2
Jason Jan <jajason_@hotmail.com>
2022-12-05 02:15:27 +0700

    edit text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
+          >babel fewfwe</a
         >
       </li>
       <li>
